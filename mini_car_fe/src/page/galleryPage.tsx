f5027202c896526f468279fe86ad35465b9dd6ae
JeonMinSu <minsu2530@u.sogang.ac.kr>
2022-12-04 03:23:48 +0900

    remove dist file

--- mini_car_fe/src/page/galleryPage.tsx
+++ mini_car_fe/src/page/galleryPage.tsx
@@ -1,19 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Image } from "../api/galleryApi";
+import { Api, DefaultPageProps } from "../App";
+import Header from "../component/header";
+import "./gallery.scss";
 
-const Preview = (src: string) => {
-    return <img src={src} />;
+const ImageComponent = (img: Image, api: Api) => {
+    return (
+        <div className="image_table_item">
+            <img className="image_table_item_img" key={img.id} src={api.s3Api.get_url(img.car_id, img.key)}></img>
+            <div></div>
+        </div>
+    );
 };
 
-interface Image {
-    id: string;
-    uploaded_time: string;
-    s3_url: string;
-}
+const VideoComponent = (img: Image, api: Api) => {
+    return (
+        <div className="image_table_item">
+            <video className="image_table_item_img" key={img.id} src={api.s3Api.get_url(img.car_id, img.key)}></video>
+            <div></div>
+        </div>
+    );
+};
 
-const GalleryPage = () => {
+const GalleryPage = ({ api, user, setUser }: DefaultPageProps) => {
     const [imgList, setImgList] = useState<Image[]>([]);
-
-    return <div></div>;
+    useEffect(() => {
+        const f = async () => {
+            const imgs = await api.galleryApi.get_img_list();
+            setImgList(imgs.data);
+            console.log(imgs.data);
+        };
+        f();
+    }, []);
+    return (
+        <>
+            <Header user={user} />
+            {imgList.length == 0 && <div>이미지가 없습니다.</div>}
+            <div className="image_table">
+                {imgList.map((img) => {
+                    if (img.type == "img") {
+                        return ImageComponent(img, api);
+                    } else {
+                        return VideoComponent(img, api);
+                    }
+                })}
+            </div>
+        </>
+    );
 };
 
 export default GalleryPage;
